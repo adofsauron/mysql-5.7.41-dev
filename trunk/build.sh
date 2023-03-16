a5933a0c0a8148c1e28bfa7420e8a5b721215748
adofsauron <adofsauron@163.com>
2023-03-16 15:00:04 +0800

    fix mysql 5.7.41

--- trunk/build.sh
+++ trunk/build.sh
@@ -5,9 +5,11 @@ cd mysql-5.7.41
 mkdir -p build
 cd build
 
+export CPPFLAGS="-DOPTIMIZER_TRACE"
+
 cmake .. \
     -DCMAKE_BUILD_TYPE=Debug \
-    -DDOWNLOAD_BOOST=1 -DWITH_BOOST=/tmp
+    -DDOWNLOAD_BOOST=1 -DWITH_BOOST=/tmp 
 
 make -j`nproc`
 make install
--- trunk/init.sh
+++ trunk/init.sh
@@ -8,6 +8,6 @@ chown -R mysql:mysql /usr/local/share/mysql/data
 
 sleep 1s
 
-/usr/local/mysql/bin/mysqld --initialize --user=mysql --datadir=/usr/local/share/mysql/data
+/usr/local/mysql/bin/mysqld --defaults-file=./my.cnf --initialize --user=mysql --datadir=/usr/local/share/mysql/data
 
 chown -R mysql:mysql /usr/local/share/mysql/data 
